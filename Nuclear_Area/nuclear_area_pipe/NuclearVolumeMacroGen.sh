c495b87da9d358244ae2dd709bd452240dc708fc
lhomas <thomas.m.litster@gmail.com>
2024-03-20 15:06:39 +1030

    Add bash pipeline for generating imageJ macros

---
+++ Nuclear_Area/nuclear_area_pipe/NuclearVolumeMacroGen.sh
@@ -0,0 +1,23 @@
+#USAGE
+# ${1} = Full path to file
+# $(2) = Transfected plasmid
+# ${3} = Path to output directory
+# ${4} = Macro Template
+
+PATH1=${1}
+PLAS=${2}
+OUT=${3}
+TEMPLATE=${4}
+
+OUTPATH=$(echo ${3}"/"${2})
+FILE=$(basename ${1})
+PREFIX=$(basename ${1} .TIF)
+
+mkdir -p ${OUTPATH}
+
+cat ${4} | \
+sed -e "s@PATH@${PATH1}@g" \
+-e "s@FILE@${FILE}@g" \
+-e "s@OUTPUT@${OUTPATH}@g" \
+-e "s@PREFIX@${PREFIX}@g" \
+> ${OUTPATH}/${PREFIX}_IJ_macro.ijm
