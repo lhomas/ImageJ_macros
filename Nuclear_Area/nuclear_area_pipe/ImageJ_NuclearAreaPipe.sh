4e286add6f4c0eccae60dddd7f1987b4f05bd4ba
lhomas <thomas.m.litster@gmail.com>
2024-03-22 12:05:35 +1030

    Updated script name of macrogen script

--- Nuclear_Area/nuclear_area_pipe/ImageJ_NuclearAreaPipe.sh
+++ Nuclear_Area/nuclear_area_pipe/ImageJ_NuclearAreaPipe.sh
@@ -32,7 +32,7 @@ echo "# Pipeline to generate macros for cell counting to generate transfection e
 }
 
 # Set path of script used to generate macro files
-MACROGEN=/Users/thomaslitster/Immunofluorescence/ImageJ_Macros/Nuclear_Area/nuclear_area_pipe/NuclearVolumeMacroGen.sh
+MACROGEN=/Users/thomaslitster/Immunofluorescence/ImageJ_Macros/Nuclear_Area/nuclear_area_pipe/NuclearAreaMacroGen.sh
 
 # Make output directory 
 mkdir -p ${3}
